465e25b075a7563f9c8a6a8d8bf60fae05edc763
Hilalbiyik <hilalbyk396@gmail.com>
2021-03-15 21:52:33 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dersler = new MyDictionary<int, string>();
+            dersler.Add(223, "Sayısal Bilgisayar Tasarımı");
+            dersler.Add(311, "Bilgisayar Mimarisi");
+            dersler.Add(323, "Web Tabanlı Programlama");
+
+            Console.Write("Seçilen dersler : ");
+
+            Console.WriteLine(dersler.Count);
+            dersler.Show();
+            Console.ReadLine();
+        }
+    }
+}
